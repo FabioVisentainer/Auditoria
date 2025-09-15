50c2217c655852f36ea4bfbb64e4a1735244d4ca
FabioVisentainer <fabio_visentainer@hotmail.com>
2025-09-14 23:34:08 -0300

    corrigido HTML

--- src/main/java/com/qualidade/auditoria/controller/DashboardController.java
+++ src/main/java/com/qualidade/auditoria/controller/DashboardController.java
@@ -1,28 +1,26 @@
 package com.qualidade.auditoria.controller;
 
-import com.qualidade.auditoria.model.Respostas;
-import com.qualidade.auditoria.repository.RespostasRepository;
+import com.qualidade.auditoria.model.Checklist;
+import com.qualidade.auditoria.repository.ChecklistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 
 import java.util.List;
-
 
 @Controller
 @RequestMapping("/")
 public class DashboardController {
 
     @Autowired
-    private RespostasRepository respostasRepository;
+    private ChecklistRepository checklistRepository;
 
     @GetMapping
-    public String listRespostas(Model model) {
-        List<Respostas> respostas = respostasRepository.findAll();
-        model.addAttribute(respostas);
-        return "/dashboard";
+    public String dashboard(Model model) {
+        List<Checklist> checklists = checklistRepository.findAll();
+        model.addAttribute("checklists", checklists);
+        return "dashboard";
     }
-
 }
